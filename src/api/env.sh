3d5a409fea7d7469672095698083557cf31c2b39
lewpar <lewis.o.parmenter@gmail.com>
2026-02-03 10:43:17 +1100

    Added dependency check / installation for fastapi to environment setup script.

--- src/api/env.sh
+++ src/api/env.sh
@@ -1,14 +1,35 @@
 ENV_PATH=".venv"
 ACTIVATE_PATH="$ENV_PATH/bin/activate"
 
-echo "Checking for environment.."
-
-if [ ! -d "$ENV_PATH" ]; then
-    echo "Environment missing, creating.."
-    python -m venv .venv
-else
-    echo "Environment found: $ENV_PATH"
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Already inside a virtual environment, deactivating.."
+    deactivate
 fi
 
-echo "Activating environment.."
+echo "Checking for virtual environment.."
+
+if [ ! -d "$ENV_PATH" ]; then
+    echo "Virtual environment missing, creating.."
+    python -m venv .venv
+    echo "> Created virtual environment at path '$ENV_PATH'."
+else
+    echo "> Virtual environment found at path: '$ENV_PATH'."
+fi
+
+echo "Activating virtual environment.."
 source $ACTIVATE_PATH
+echo "> Virtual enviroment activated."
+
+echo "Checking for installed dependencies.."
+
+pip show --quiet "fastapi"
+
+# $? returns the exit code for the last command run.
+if [[ ! $?  ]]; then
+    echo "FastAPI dependency missing, installing.."
+    pip install --quiet "fastapi[standard]"
+fi
+
+echo "> Done checking dependencies."
+
+echo "> Completed setting up virtual environment."
