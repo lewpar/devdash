82f03e92a078cac6c607f330f12738a1cee38ee5
lewpar <lewis.o.parmenter@gmail.com>
2026-02-03 18:56:36 +1100

    Removed unused endpoints.

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 
 import routers.memory
@@ -6,13 +5,3 @@ import routers.memory
 app = FastAPI()
 
 app.include_router(routers.memory.router)
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
