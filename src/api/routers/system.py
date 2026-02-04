1c85266b15c7d70e01fb490fdfc2c3c3ec70e2a4
lewpar <lewis.o.parmenter@gmail.com>
2026-02-04 12:53:27 +1100

    Renamed endpoints and added cpu frequency.

--- src/api/routers/system.py
+++ src/api/routers/system.py
@@ -4,8 +4,8 @@ import psutil
 
 router = APIRouter(prefix="/system")
 
-@router.get("/memory")
-def get_memory():
+@router.get("/memory/usage")
+def get_memory_usage():
     memory = psutil.virtual_memory()
     return {
         "unit": "bytes",
@@ -14,12 +14,22 @@ def get_memory():
         "total": memory.total
     }
 
-@router.get("/disk")
-def get_disk():
+@router.get("/disk/usage")
+def get_disk_usage():
     disk = psutil.disk_usage("/")
     return {
         "unit": "bytes",
         "free": disk.free,
         "used": disk.used,
         "total": disk.total
+    }
+
+@router.get("/cpu/frequency")
+def get_cpu_frequency():
+    cpu = psutil.cpu_freq()
+    return {
+        "unit": "Mhz",
+        "min": cpu.min,
+        "max": cpu.max,
+        "current": cpu.current
     }
