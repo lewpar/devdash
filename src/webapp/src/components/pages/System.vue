3a22826a6c1a4be7afc1f1ca87a4f38e102ad5f6
lewpar <lewis.o.parmenter@gmail.com>
2026-02-06 14:05:31 +1100

    Added disk usage.

--- src/webapp/src/components/pages/System.vue
+++ src/webapp/src/components/pages/System.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import { ProgressSpinner } from 'primevue';
+import { ProgressSpinner, type MeterItem } from 'primevue';
 import CPUUsage from '../system/CPUUsage.vue';
+import DiskUsage from '../system/DiskUsage.vue';
 
 interface CPUInfo {
     frequency: {
@@ -21,7 +22,21 @@ interface CPUInfo {
     }
 }
 
+interface DiskUsage {
+    unit: string;
+    free: number;
+    used: number;
+    total: number;
+}
+
 const cpuInfo = ref<CPUInfo | null>(null);
+const diskUsage = ref<MeterItem[] | null>(null);
+const diskMax = ref<number>(100);
+
+async function getSystemResources() {
+    await getCPUInfo();
+    await getDiskUsage();
+}
 
 async function getCPUInfo() {
     const result = await fetch("http://localhost:8000/system/cpu");
@@ -30,16 +45,42 @@ async function getCPUInfo() {
     cpuInfo.value = info;
 }
 
+async function getDiskUsage() {
+    const result = await fetch("http://localhost:8000/system/disk/usage");
+    const usage = (await result.json()) as DiskUsage;
+
+    diskUsage.value = [
+        {
+            label: "Used",
+            value: usage.used,
+            color: "rgb(99 182 134)"
+        },
+        {
+            label: "Free",
+            value: usage.free,
+            color: "#e4e8ef"
+        }
+    ];
+
+    diskMax.value = usage.total;
+}
+
 onMounted(async () => {
     await getCPUInfo();
+    await getDiskUsage();
 
-    setInterval(getCPUInfo, 2500);
+    setInterval(getSystemResources, 2500);
 });
 </script>
 
 <template>
     <div v-if="cpuInfo">
         <CPUUsage :usage="cpuInfo.usage"/>
+    </div>
+    <ProgressSpinner v-else/>
+
+    <div v-if="diskUsage">
+        <DiskUsage :usage="diskUsage" :max="diskMax"/>
     </div>
     <ProgressSpinner v-else/>
 </template>
---
+++ src/webapp/src/components/system/DiskUsage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { MeterGroup, type MeterItem } from 'primevue';
+
+const props = defineProps<{
+    usage: MeterItem[],
+    max: number
+}>();
+</script>
+
+<template>
+    <div id="disk-usage">
+        <h4>Disk Usage</h4>
+        <MeterGroup :value="props.usage" :max="props.max" />
+    </div>
+</template>
+
+<style scoped>
+#disk-usage {
+    display: flex;
+    flex-direction: column;
+}
+</style>
