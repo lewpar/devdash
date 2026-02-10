ebd32eee96a9423367dcab20bd883478b1f67159
lewpar <lewis.o.parmenter@gmail.com>
2026-02-10 18:48:04 +1100

    Added creation and deletion endpoint for docker.

--- src/api/routers/dogger.py
+++ src/api/routers/dogger.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 import docker
 
@@ -7,5 +8,29 @@ router = APIRouter(prefix="/docker", tags=["Docker"])
 @router.get("/containers")
 def get_docker_containers():
     client = docker.from_env()
-    containers = client.containers
-    return containers.list()
+    containers = client.containers.list(all=True)
+    serializable_containers = []
+    for container in containers:
+        serializable_containers.append({
+            "id": container.id
+        })
+    return serializable_containers
+
+@router.get("/create")
+def create_docker_container():
+    client = docker.from_env()
+    container = client.containers.create("hello-world")
+    client.containers.run("hello-world")
+    return {
+        "id": container.id
+    }
+
+class DeleteDockerContainerModel(BaseModel):
+    id: str
+
+@router.post("/delete")
+def delete_docker_container(model: DeleteDockerContainerModel):
+    client = docker.from_env()
+    container = client.containers.get(model.id)
+    container.remove()
+    return "removed"
